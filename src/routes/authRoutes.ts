dd8d92af3c21f50113a7e8abc83eb8bfa8eba0d8
ispastro <haileasaye51@gmail.com>
2025-07-25 04:04:30 +0300

    authRoutes is done

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,12 @@
+// src/routes/authRoutes.ts
+import express from 'express';
+import { signup, login, getProfile } from '../controllers/authController';
+import { authenticate } from '../middleware/authMiddleware';
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/profile', authenticate, getProfile);
+
+export default router;
