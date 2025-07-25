e0eb5ac55ac9cb2eba326a63e4bb2a185edb3a1a
ispastro <haileasaye51@gmail.com>
2025-07-25 04:05:05 +0300

    tasks routes is done

---
+++ src/routes/taskRoutes.ts
@@ -0,0 +1,13 @@
+// src/routes/taskRoutes.ts
+import express from 'express';
+import { createTask, getTasks, updateTask, deleteTask } from '../controllers/taskController';
+import { authenticate } from '../middleware/authMiddleware';
+
+const router = express.Router();
+
+router.post('/tasks', authenticate, createTask);
+router.get('/tasks', authenticate, getTasks);
+router.patch('/tasks/:id', authenticate, updateTask);
+router.delete('/tasks/:id', authenticate, deleteTask);
+
+export default router;
